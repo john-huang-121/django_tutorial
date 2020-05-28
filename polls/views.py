4da9b64cf8562abfbc66933dee16200b583cb1f5
john <johnhuang121@gmail.com>
2020-05-27 23:02:15 -0700

    rework detail method to handle exception

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
+# shortcut 'render' for simplifying the loader process
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -13,7 +19,11 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!")
+    return render(request, 'polls/detail.html', {'question':question})
   
 def results(request, question_id):
     response = "You're looking at the results of question %s."
