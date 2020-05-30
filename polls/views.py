3c77ddb9030576a2df2ba1d294679171953db1e8
john <johnhuang121@gmail.com>
2020-05-29 18:00:40 -0700

    namespacing index.html in poll app

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse, Http404, get_object_or_404
+from django.http import HttpResponse, Http404
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
