ea95c1491206f4c95c59caf928d652c83b3c4551
john-huang-121 <johnhuang121@gmail.com>
2020-06-01 15:14:13 -0700

    add question admin filter search and fieldsets

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
+from .models import Question, Choice
 # Register your models here.
-from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        ('Date information', {'fields': ['pub_date']}),
+        (None,               {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
